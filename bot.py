ac5a336a8f840a50047f4a55ba447fed339366ad
r00 <15057809+User-r00@users.noreply.github.com>
2020-10-07 03:18:30 -0700

    Begin rework

--- bot.py
+++ bot.py
@@ -1,44 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Main bot file for Kappa Deep."""
-
+import os
 from twitchio.ext import commands
-
-from config import config
-from tokens import tokens
-
-startup_extensions = ['extensions.general',
-                      'extensions.obs',
-                      'extensions.sfx',
-                      'extensions.skyline',
-                      'extensions.streamelements']
-
-bot = commands.Bot(irc_token=tokens.TWITCH_TOKEN,
-                   nick=config.NICK,
-                   prefix=config.PREFIX,
-                   initial_channels=[config.CHAN])
+from dotenv import load_dotenv
+import subprocess
 
 
-@bot.event
-async def event_ready():
-  """Run when bot loads."""
-  msg = f'{config.NICK} ready for duty! Batteries not included.'
-  print(msg)
+# Load environment variables
+load_dotenv()
 
-@bot.event
-async def event_message(message):
-  """Print messages."""
-  # print(message._raw_data)
+class Bot(commands.Bot):
 
-  await bot.handle_commands(message)
+    def __init__(self):
+        super().__init__(irc_token=os.environ['IRC_TOKEN'],
+                         client_id=os.environ['CLIENT_ID'],
+                         nick=os.environ['BOT_NICK'],
+                         prefix=os.environ['BOT_PREFIX'],
+                         initial_channels=[[os.environ['CHANNEL']]])
 
-if __name__ == '__main__':
-    for extension in startup_extensions:
-        try:
-            bot.load_module(extension)
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print(f'[ERR] Can\'t load extension {extension}\n{exc}')
+    # Events don't need decorators when subclassed
+    async def event_ready(self):
+        print(f'Ready | {self.nick}')
+        ws = bot._ws  # this is only needed to send messages within event_ready
+        await ws.send_privmsg(os.environ['CHANNEL'], f'/me is here.')
 
-bot.run()
+    async def event_message(self, message):
+        print(message.content)
+        await self.handle_commands(message)
+
+    # Commands use a decorator...
+    @commands.command(name='test')
+    async def my_command(self, ctx):
+        await ctx.send(f'Hello {ctx.author.name}!')
+        
+    @commands.command(name='reboot')
+    async def reboot_command(self, ctx):
+        await ctx.send('Rebooting. Time me.')
+        
+        cmd = subprocess.run(['sudo', 'shutdown', '-r', 'now'])
+        if cmd.returncode != 0:
+            await ctx.send('JK. I can\'t reboot for...reasons. Check my logs.')
+        
+if __name__ == "__main__":
+    bot = Bot()
+    bot.run()
